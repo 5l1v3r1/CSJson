c6d755c0d6e739e011924ee8109791ea3b56e9f0
Techcraft7 <35858721+Techcraft7@users.noreply.github.com>
2019-01-28 10:47:25 -0500

    the begining code
    prevents from loading more than 1 file

--- csjson.cs
+++ csjson.cs
@@ -8,7 +8,20 @@ namespace CSJson
 	{
 		public static void Main(string[] args)
 		{
-			
+			if (args.Length == 0)
+			{
+				Console.WriteLine("You did not specify a file!");
+			}
+			else if (args.Length == 1)
+			{
+				#if DebugConfig  
+        				Console.WriteLine("Debug Mode:\nOnly 1 file specified... continuing...");  
+				#endif 
+			}
+			else
+			{
+				Console.WriteLine("Dont specify more than 1 file!");
+			}
 		}
 	}
 }
